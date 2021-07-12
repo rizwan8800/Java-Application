4e37f46acb62ebb525fdc7c1c6ed828145f92899
mrkhanboyy <mrkhanboyy@gmail.com>
2021-07-12 10:48:30 +0530

    second commit

--- CoreJava/src/com/training/org/Employee.java
+++ CoreJava/src/com/training/org/Employee.java
@@ -3,5 +3,6 @@ package com.training.org;
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("message from employee");
+		System.out.println("message-2 from employee");
 	}
 }
